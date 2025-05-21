00186a21a506ca44326c1889270d18332b37e910
ArsMaster <G.arsen90@gmail.com>
2025-05-21 21:03:24 +0300

    Third task is done Arsen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,46 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+const newPages : number[] = [5,4,3,2,1]
+
+const menuItems : string[] = ['Каталог', 'Стройматериалы', 'Инструменты', 'Электрика', 'Интерьер и одежда']
+const upperCasemenuItems : string[] = menuItems.map(
+  (item : string) => {
+    return item.toUpperCase()
+  }
+)
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'mentoring-first-project';
+
+  isShowCatalog : boolean = true;
+  isShowAboutCompany : boolean = true;
+
+  readonly navItem1 : "Главная" = 'Главная';
+  readonly aboutCompany : "О компании" = 'О компании';
+  readonly navItem3 : "Каталог" = 'Каталог';
+
+  isShowImg : boolean = true;
+
+  readonly newPages : number[] = newPages;
+
+  menuItems : string[] = upperCasemenuItems;
+
+  isUpperCase : boolean = true;
+
+  changeMenuText(): void {
+    this.menuItems = upperCasemenuItems.map(
+      item => this.isUpperCase ? item.toLowerCase() : item.toUpperCase()
+    )
+
+    this.isUpperCase = !this.isUpperCase
+  }
 }
